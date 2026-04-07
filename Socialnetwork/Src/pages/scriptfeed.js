e3871a1f72dcd477e44c4f6c4a965318010ecf59
Vanessa Nuñez <vnunezte@gmail.com>
2026-04-07 17:15:56 -0500

    feat: implement comment screen, interactive stars and margin adjustment

--- Socialnetwork/Src/pages/scriptfeed.js
+++ Socialnetwork/Src/pages/scriptfeed.js
@@ -24,3 +24,31 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+const pantalla = document.getElementById("pantalla-comentarios");
+
+document.querySelectorAll(".abrir-comentarios").forEach((icon) => {
+  icon.addEventListener("click", () => {
+    pantalla.classList.remove("oculto");
+  });
+});
+
+function cerrarComentarios() {
+  pantalla.classList.add("oculto");
+}
+document.querySelectorAll(".stars").forEach((grupo) => {
+  const estrellas = grupo.querySelectorAll("i");
+
+  estrellas.forEach((estrella, index) => {
+    estrella.addEventListener("click", () => {
+      estrellas.forEach((e, i) => {
+        if (i <= index) {
+          e.classList.remove("fa-regular");
+          e.classList.add("fa-solid");
+        } else {
+          e.classList.remove("fa-solid");
+          e.classList.add("fa-regular");
+        }
+      });
+    });
+  });
+});
